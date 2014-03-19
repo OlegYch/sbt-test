2080a00915a0b4152647e932fbbeb7bbc917cec9
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2014-03-19 15:04:09 +0100

    Perturb things trying to trigger the bug
    Didn't work, but negative results are also useful (for people not to try
    again the same thing).

--- src/main/scala/A.scala.mod
+++ src/main/scala/A.scala.mod
@@ -5,6 +5,3 @@ trait VersionedCachedMap[K <: AnyRef, V, M[_]] extends BaseCachedMap[K, V, M] {
   private val a = new Object
   abstract override def get:Int = {List().foreach{_ => println(a); super.get}; 1}
 }
-case class CachedMap[K, V]() extends BaseCachedMap[K, V, Option] {
-  def get: Int = 1
-}
--- src/main/scala/B.scala
+++ src/main/scala/B.scala
@@ -1,3 +1,7 @@
+case class CachedMap[K, V]() extends BaseCachedMap[K, V, Option] {
+  def get: Int = 1
+}
+class C() extends CachedMap[String, String]() with VersionedCachedMap[String, String, Option]
 object B extends App {
-  new CachedMap[String, String]() with VersionedCachedMap[String, String, Option]
+  new C().get
 }
